b76e61d1b14717033a710133479a04206f6a17ad
Spiros Makris <makris.spiros@gmail.com>
2020-08-21 15:42:06 +0300

    Started working on map generation

--- src/plugins/terrain.rs
+++ src/plugins/terrain.rs
@@ -8,6 +8,64 @@ use bevy::render::{
 
 use crate::{GridPos, CHUNK_WORLD_SIZE};
 
+const CHUNK_WIDTH_TILES: i32 = 32;
+const CHUNK_HEIGHT_TILES: i32 = 32;
+
+#[derive(Debug)]
+pub struct Tile {
+    pub tile_id: u32,
+    pub pos: Vec2,
+}
+
+#[derive(Debug)]
+pub struct Chunk {
+    pub position: Vec2,
+    pub tiles: Vec<Vec<Tile>>,
+}
+
+#[derive(Debug)]
+pub struct Map {}
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum TerrainType {
+    Floor,
+    Wall,
+}
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * CHUNK_WIDTH_TILES as usize) + x as usize
+}
+
+fn new_chunk_tiles() -> Vec<TerrainType> {
+    let mut map =
+        vec![TerrainType::Floor; CHUNK_WIDTH_TILES as usize * CHUNK_HEIGHT_TILES as usize];
+
+    // Make the boundaries walls
+    for x in 0..CHUNK_WIDTH_TILES {
+        map[xy_idx(x, 0)] = TerrainType::Wall;
+        map[xy_idx(x, CHUNK_WIDTH_TILES - 1)] = TerrainType::Wall;
+    }
+
+    for y in 0..CHUNK_HEIGHT_TILES {
+        map[xy_idx(0, y)] = TerrainType::Wall;
+        map[xy_idx(CHUNK_HEIGHT_TILES - 1, y)] = TerrainType::Wall;
+    }
+
+    // Randomly splat a bunch of walls
+    use bracket_random::prelude::*;
+    let mut rng = RandomNumberGenerator::new();
+
+    for _i in 0..map.len() {
+        let x = rng.roll_dice(1, CHUNK_WIDTH_TILES - 1);
+        let y = rng.roll_dice(1, CHUNK_HEIGHT_TILES - 1);
+        let idx = xy_idx(x, y);
+
+        map[idx] = TerrainType::Wall;
+    }
+
+    map
+}
+
 /// A component bundle for a TerrainChunk
 /// Based on PBRComponents
 #[derive(Bundle)]
@@ -62,7 +120,8 @@ pub struct TerrainPlugin;
 
 impl Plugin for TerrainPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_startup_system(ss_setup_terrain.system());
+        app.add_startup_system(ss_setup_terrain.system())
+            .add_startup_system(ss_test_chunking.system());
     }
 }
 
@@ -71,14 +130,7 @@ fn ss_setup_terrain(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // commands.spawn(PbrComponents {
-    //     mesh: meshes.add(Mesh::from(shape::Plane {
-    //         size: CHUNK_WORLD_SIZE,
-    //     })),
-    //     material: materials.add(Color::rgb(0.1, 0.2, 0.1).into()),
-    //     ..Default::default()
-    // });
-    let grid_pos = GridPos { x: 2, y: 1 };
+    let grid_pos = GridPos { x: 0, y: 0 };
     let translation = Translation::new(
         grid_pos.x as f32 * CHUNK_WORLD_SIZE,
         0.0,
@@ -94,4 +146,13 @@ fn ss_setup_terrain(
         translation,
         ..Default::default()
     });
+}
+
+fn ss_test_chunking(resources: &mut Resources) {
+    let chunky = new_chunk_tiles();
+    println!("CHUNKY: {:?}", chunky);
+    println!("CHUNKY len = {}", chunky.len());
+
+    // Insert map resource
+    resources.insert(chunky);
 }
